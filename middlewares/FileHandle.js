12e7cb9c5ec16fc90fb1a15ed86975041b7ca9f1
imimran <engr.aih@gmail.com>
2021-03-15 22:47:37 +0600

    File hHangle with Multer

--- middlewares/FileHandle.js
+++ middlewares/FileHandle.js
@@ -1,17 +1,30 @@
 const multer = require('multer');
+const express = require('express')
+const path = require('path')
 
+const app = express()
 //configuring multer storage for images
 const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'upload/');
     },
     filename: (req, file, cb) => {
-        cb(null, new Date().toISOString().replace(/:/g, '-')
-            + '-' + file.originalname);
+        // cb(null, new Date().toISOString().replace(/:/g, '-')
+        //     + '-' + file.originalname);
+
+        //Imprtent File.jpg => importent-file-5476585.jpg
+        const fileExt = path.extname(file.originalname)  //remove extntion
+        const fileName = file.originalname
+            .replace(fileExt, "")
+            .toLowerCase()
+            .split(' ')
+            .join('-') + "-" + Date.now()
+        cb(null, fileName + fileExt)
+
     }
 });
 
-//filtering images
+//Filtering images for every file Field
 const fileFilter = (req, file, cb) => {
     if (file.mimetype === 'image/png' ||
         file.mimetype === 'image/x-icon' ||
@@ -20,28 +33,46 @@ const fileFilter = (req, file, cb) => {
         file.mimetype === 'image/jpeg') {
         cb(null, true);
     } else {
-        cb(null, false);
+        // cb(null, false);
+        cb(new Error("Only .jpg, .png, .jpeg, x-icon or .gif formet allowed !"));
     }
 };
 
-const upload = multer({ storage: fileStorage, fileFilter: fileFilter });
+
+
+const upload = multer({
+    storage: fileStorage,
+    fileFilter: fileFilter,
+    limits: { fileSize: 1000000 } //1 MB
+});
+
 const formOnly = multer();
 
 const multipleFile = upload.fields([
-    { name: 'faviconFile', maxCount: 1 },
-    { name: 'mainLogo', maxCount: 1 },
-    { name: 'stickyLogo', maxCount: 1 },
-    { name: 'footerLogo', maxCount: 1 },
-    { name: 'notFoundFile', maxCount: 1 },
-    { name: 'notFoundBgFile', maxCount: 1 },
-    { name: 'footerBgFile', maxCount: 1 },
- 
+    { name: 'logo', maxCount: 1 },
+    { name: 'gallery', maxCount: 3 },
+
 ]);
+
+
 const uploadFile = upload.single('image');
 const uploadAvatar = upload.single('avatar');
+const multiFileUploader = upload.array('avatar', 3)
 const uploadCoverPhoto = upload.single('cover_photo');
 const form = formOnly.none();
 
+//Multer Error Handle
+app.use((err, req, res, next) => {
+    if (err) {
+        if (err instanceof multer.MulterError) {
+            res.status(500).send('There was an upload error!')
+        } else {
+            res.status(500).send(err.message)
+        }
+    } else {
+        res.status(200).json({ "success"})
+    }
 
+})
 // app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
-module.exports = { multipleFile, uploadAvatar, uploadFile, uploadCoverPhoto, form }
+module.exports = { multipleFile, multiFileUploader, uploadAvatar, uploadFile, uploadCoverPhoto, form }
